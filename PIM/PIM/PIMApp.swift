b057f8a2cd201c576c04feb4deec9738ec5bb1e9
MADElinessss <jyseen@naver.com>
2023-09-16 16:10:34 +0900

    projectfile 생성

---
+++ PIM/PIM/PIMApp.swift
@@ -0,0 +1,20 @@
+//
+//  PIMApp.swift
+//  PIM
+//
+//  Created by 신정연 on 2023/09/16.
+//
+
+import SwiftUI
+
+@main
+struct PIMApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
