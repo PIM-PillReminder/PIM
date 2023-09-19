796fdbc806e3c1162b94434d0e81b49e3677920b
MADElinessss <jyseen@naver.com>
2023-09-19 20:20:32 +0900

    [feat] notification 5분전, 1시간 후 추가
    -헤 까먹고 안해서 기능 다시 추가해용

--- PIM/PIM/LocalNotificationManager.swift
+++ PIM/PIM/LocalNotificationManager.swift
@@ -26,9 +26,10 @@ class LocalNotificationManager {
     }
     
     func addNotification(title: String) -> Void {
-//        notifications.append(Notification(id: UUID().uuidString, title: title))
         let notification = Notification(id: UUID().uuidString, title: title)
         notifications.append(notification)
+        
+         //notification -> 꾹 누르면 noti action으로 버튼 생성
         
         let checkAction = UNNotificationAction(
         identifier: "checkAction",
@@ -60,7 +61,7 @@ class LocalNotificationManager {
     func scheduleNotifications() -> Void {
         for notification in notifications {
             var dateComponents = Calendar.current.dateComponents([.hour, .minute], from: Date()) // 기본 값 (현재 시간) 가져옴
-
+            
             if let selectedTime = UserDefaults.standard.object(forKey: "SelectedTime") as? Date {
                 let calendar = Calendar.current
                 let selectedHour = calendar.component(.hour, from: selectedTime)
@@ -68,22 +69,58 @@ class LocalNotificationManager {
                 dateComponents.hour = selectedHour
                 dateComponents.minute = selectedMinute
             }
-
-            dateComponents.second = 0 // 초를 0으로 설정합니다.
-
+            
+            dateComponents.second = 0
+            
+            //정시 알림
             let content = UNMutableNotificationContent()
             content.title = notification.title
-            content.sound = UNNotificationSound.default
+            //            content.sound = UNNotificationSound.default
             content.subtitle = "약 먹을 시간이에요.💊"
             content.body = "먹었다고 체크하기"
             content.categoryIdentifier = "checkCategory"
-
+            
             let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
             let request = UNNotificationRequest(identifier: notification.id, content: content, trigger: trigger)
-
+            
             UNUserNotificationCenter.current().add(request) { error in
                 guard error == nil else { return }
                 print("Scheduling notification with id:\(notification.id)")
+            }
+            
+            //5분 전 알림
+            dateComponents.minute! -= 5;
+            let before5MinutesContent = UNMutableNotificationContent()
+            before5MinutesContent.title = "약을 먹을 시간이 얼마 남지 않았어요!"
+            before5MinutesContent.sound = UNNotificationSound.default
+            before5MinutesContent.subtitle = "약 먹을 시간이에요.💊"
+            before5MinutesContent.body = "먹었다고 체크하기"
+            before5MinutesContent.categoryIdentifier = "checkCategory"
+            
+            let before5MinutesTrigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+            let before5MinutesRequest = UNNotificationRequest(identifier: notification.id + "_5min", content: before5MinutesContent, trigger: before5MinutesTrigger)
+            
+            UNUserNotificationCenter.current().add(request) { error in
+                guard error == nil else { return }
+                print("Scheduling 5 minutes before notification with id:\(notification.id + "_5min")")
+            }
+            
+            //1시간 후 알림
+            dateComponents.minute! += 65;
+            let after1HourContent = UNMutableNotificationContent()
+            after1HourContent.title = "1시간이 지났어요."
+            before5MinutesContent.sound = UNNotificationSound.default
+            after1HourContent.subtitle = "괜찮아요. 지금 약을 먹으세요."
+            after1HourContent.body = "먹었다고 체크하기"
+            after1HourContent.categoryIdentifier = "checkCategory"
+            
+            let after1HourTrigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+            let after1HourRequest = UNNotificationRequest(identifier: notification.id
+                                                           + "_1hour", content: after1HourContent, trigger: after1HourTrigger)
+            
+            UNUserNotificationCenter.current().add(request) { error in
+                guard error == nil else { return }
+                print("Scheduling 1 hour after notification with id:\(notification.id + "_5min")")
             }
         }
     }
