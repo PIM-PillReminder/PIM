a5a9fd6fe485722e2be13b1180ddcef3a19cb6b6
MADElinessss <jyseen@naver.com>
2023-09-23 17:53:54 +0900

    [design] 온보딩~메인뷰 디자인 수정

--- PIM/PIM/MainView.swift
+++ PIM/PIM/MainView.swift
@@ -21,6 +21,7 @@ struct MainView: View {
                 // TODO: SettingView로 연결
                 NavigationLink(destination: MainView()) {
                     Image(systemName: "gearshape")
+                        .font(.system(size: 24))
                         .foregroundColor(Color.black)
                         .padding(.leading, 20)
                 }
@@ -32,9 +33,20 @@ struct MainView: View {
                 // TODO: CalendarView로 연결
                 NavigationLink(destination: MainView()) {
                     Image(systemName: "calendar")
+                        .font(.system(size: 24))
                         .padding(.trailing, 20)
                         .foregroundColor(Color.black)
                 }
+            }
+            .padding(.top, 10)
+            Spacer()
+            VStack{
+                Image("pill")
+                    .padding(.bottom, 10)
+                Text(isPillEaten ? "약을 먹었어요\n오늘도 힘차는 하루!" : "오늘의 약을 아직 안 먹었어요")
+                    .font(.system(size: 24))
+                    .multilineTextAlignment(.center)
+                    .fontWeight(.bold)
             }
             Spacer()
             Image("character_onboarding_big")
@@ -57,15 +69,20 @@ struct MainView: View {
 //                }
 //            }
             Button(action: {
-                
+                isPillEaten = true
             }) {
                 ZStack {
                     Rectangle()
                         .fill(Color.gray)
                         .cornerRadius(16)
-                        .frame(width: UIScreen.main.bounds.width * 0.8, height: 60)
+                        .frame(width: UIScreen.main.bounds.width * 0.9, height: 60)
                         .padding(.top, 40)
                         .padding(.bottom, 10)
+                    Text("오늘의 약을 복용 했어요")
+                        .font(.system(size: 18))
+                        .fontWeight(.medium)
+                        .foregroundColor(Color.black)
+                        .padding(.top, 30)
                 }
             }
 
