e4a93b471c18873fcbd926a72d578c9d8bf6c059
Lorenzo <tnalsdlekgsk1@icloud.com>
2024-09-09 03:26:40 +0900

    [feat] - 약 복용 여부 배열로 저장&불러오도록 변경

--- PIM/PIM/FirestoreManager.swift
+++ PIM/PIM/FirestoreManager.swift
@@ -9,6 +9,9 @@ import Foundation
 import FirebaseFirestore
 
 class FireStoreManager: ObservableObject {
+    @Published var pillStatusList: [PillStatus] = []
+    @Published var notificationTime: String?
+    
     var documentID: String? {
         get {
             return UserDefaults.standard.string(forKey: "documentID")
@@ -17,13 +20,29 @@ class FireStoreManager: ObservableObject {
             UserDefaults.standard.set(newValue, forKey: "documentID")
         }
     }
-    @Published var isPillEaten: Bool?
-    @Published var notificationTime: Date?
     
-    init() {
+    func createData(notificationTime: String, pillStatus: PillStatus) async throws {
+        let db = Firestore.firestore()
+        
+        do {
+            // 새로운 문서 생성
+            let ref = try await db.collection("userData").addDocument(data: [
+                "notificationTime": notificationTime,
+                "pillDataArray": [pillStatus.toDictionary()]
+            ])
+            
+            // 생성된 documentID를 UserDefaults에 저장
+            UserDefaults.standard.set(ref.documentID, forKey: "documentID")
+            self.documentID = ref.documentID
+            print("Document created with ID: \(ref.documentID)")
+            
+        } catch {
+            print("Error creating document: \(error)")
+            throw error
+        }
     }
     
-    //MARK: 파이어스토어에서 값 가져오는 함수
+    // Firestore에서 데이터를 가져오는 함수
     func fetchData() {
         guard let documentID = self.documentID else {
             print("Document ID is nil")
@@ -31,6 +50,7 @@ class FireStoreManager: ObservableObject {
         }
         let db = Firestore.firestore()
         let docRef = db.collection("userData").document(documentID)
+        
         docRef.getDocument { (document, error) in
             guard error == nil else {
                 print("Error fetching document:", error ?? "")
@@ -39,39 +59,26 @@ class FireStoreManager: ObservableObject {
             
             if let document = document, document.exists {
                 let data = document.data()
-                if let data = data {
-                    if let notificationTimeTimestamp = data["notificationTime"] as? Timestamp {
-                        self.notificationTime = notificationTimeTimestamp.dateValue()
-                        print("Notification Time:", self.notificationTime ?? "N/A")
-                    } else {
-                        self.notificationTime = nil
-                        print("Notification Time is nil")
+                
+                // Firestore에서 가져온 pillDataArray 배열을 변환
+                if let pillDataArray = data?["pillDataArray"] as? [[String: Any]] {
+                    self.pillStatusList = pillDataArray.map { dict in
+                        let isPillEaten = dict["isPillEaten"] as? Bool ?? false
+                        let pillDate = dict["pillDate"] as? String ?? ""
+                        return PillStatus(isPillEaten: isPillEaten, pillDate: pillDate)
                     }
-                    self.isPillEaten = data["isPillEaten"] as? Bool
-                    print("Is Pill Eaten:", self.isPillEaten ?? "N/A")
+                }
+                
+                // Firestore에서 가져온 notificationTime을 변환
+                if let notificationTime = data?["notificationTime"] as? String {
+                    self.notificationTime = notificationTime
                 }
             }
         }
     }
 
-    
-    
-    func createData(notificationTime: Date, isPillEaten: Bool = false) async {
-        let db = Firestore.firestore()
-        do {
-            let ref = try await db.collection("userData").addDocument(data: [
-                "notificationTime": notificationTime,
-                "isPillEaten": isPillEaten])
-            // UserDefaults에 document ID 저장
-            UserDefaults.standard.set(ref.documentID, forKey: "documentID")
-            self.documentID = ref.documentID
-            print("Document added with ID: \(ref.documentID)")
-        } catch {
-            print("Error adding document: \(error)")
-        }
-    }
-    
-    func updateIsPillEaten(isPillEaten: Bool) {
+    // Firestore에 PillStatus 배열 저장하는 함수
+    func savePillStatus(pillStatus: PillStatus) {
         guard let documentID = self.documentID else {
             print("Document ID is nil")
             return
@@ -80,18 +87,56 @@ class FireStoreManager: ObservableObject {
         let db = Firestore.firestore()
         let docRef = db.collection("userData").document(documentID)
         
-        docRef.updateData([
-            "isPillEaten": isPillEaten
-        ]) { error in
-            if let error = error {
-                print("Error updating isPillEaten: \(error)")
-            } else {
-                print("isPillEaten successfully updated")
-                // 업데이트가 성공했을 때 추가 작업을 수행할 수 있습니다.
+        let pillStatusData = pillStatus.toDictionary()
+
+        // 기존 날짜와 동일한 데이터를 삭제 (arrayRemove)
+        docRef.getDocument { (document, error) in
+            guard let document = document, document.exists else {
+                print("Document does not exist.")
+                return
+            }
+            
+            if let data = document.data(), let pillDataArray = data["pillDataArray"] as? [[String: Any]] {
+                // 동일한 날짜의 데이터가 있는지 확인
+                if let existingPillData = pillDataArray.first(where: { $0["pillDate"] as? String == pillStatus.pillDate }) {
+                    // 동일한 날짜의 데이터를 arrayRemove로 삭제
+                    docRef.updateData([
+                        "pillDataArray": FieldValue.arrayRemove([existingPillData])
+                    ]) { error in
+                        if let error = error {
+                            print("Error removing existing pill status: \(error)")
+                        } else {
+                            print("Existing pill status successfully removed.")
+                            // 삭제 후 새 데이터를 arrayUnion으로 추가
+                            docRef.updateData([
+                                "pillDataArray": FieldValue.arrayUnion([pillStatusData])
+                            ]) { error in
+                                if let error = error {
+                                    print("Error updating pill status: \(error)")
+                                } else {
+                                    print("Pill status successfully updated.")
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    // 동일한 날짜의 데이터가 없으면 바로 추가
+                    docRef.updateData([
+                        "pillDataArray": FieldValue.arrayUnion([pillStatusData])
+                    ]) { error in
+                        if let error = error {
+                            print("Error adding pill status: \(error)")
+                        } else {
+                            print("Pill status successfully added.")
+                        }
+                    }
+                }
             }
         }
     }
-    
+
+
+    // Firestore에 알림 시간을 저장하는 함수
     func updateNotificationTime(notificationTime: Date) {
         guard let documentID = self.documentID else {
             print("Document ID is nil")
@@ -101,16 +146,15 @@ class FireStoreManager: ObservableObject {
         let db = Firestore.firestore()
         let docRef = db.collection("userData").document(documentID)
         
+        // Firestore에 Date 타입으로 저장
         docRef.updateData([
             "notificationTime": notificationTime
         ]) { error in
             if let error = error {
-                print("Error updating notificationTime: \(error)")
+                print("Error updating notification time: \(error)")
             } else {
-                print("notificationTime successfully updated")
-                // 업데이트가 성공했을 때 추가 작업을 수행할 수 있습니다.
+                print("Notification time successfully updated")
             }
         }
     }
-    
 }
--- PIM/PIM/View/MainView.swift
+++ PIM/PIM/View/MainView.swift
@@ -120,8 +120,12 @@ struct MainView: View {
                     }
                 }
             }
-            .onChange(of: pillStatusObserver.isPillEaten) {
-                firestoreManager.updateIsPillEaten(isPillEaten: pillStatusObserver.isPillEaten)
+            .onChange(of: pillStatusObserver.isPillEaten) { newValue in
+                // 현재 시간과 함께 새로운 PillStatus 객체를 생성
+                let newPillStatus = PillStatus(isPillEaten: newValue, pillDate: Date().getFormattedDate())
+                
+                // FirestoreManager에 새 PillStatus 객체 저장
+                firestoreManager.savePillStatus(pillStatus: newPillStatus)
             }
             .navigationBarBackButtonHidden(true)
             .navigationTitle("")
